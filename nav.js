0ec28e62e8065754ab6d49914e2bf12229b1e9a1
amitomer <amitomer312@gmail>
2018-08-22 13:24:19 +0300

    My navigator

---
+++ nav.js
@@ -0,0 +1,23 @@
+
+$("#Home").click (function (){
+    $("#content").empty();
+    const html = "<br><p>Hello! You've reached Home!<br> Please, take off your shoes.</p>";
+    $("#content").append(html);
+});
+
+$("#Page1").click (function (){
+    $("#content").empty();
+    const html = "<br><p>Hello! You've reached Page1!</p>";
+    $("#content").append(html);
+});
+
+$("#Page2").click (function (){
+    $("#content").empty();
+    const html = "<br><p>Hello! You've reached Page2!</p>";
+    $("#content").append(html);
+});
+$("#Page3").click (function (){
+    $("#content").empty();
+    const html = "<br><p>Hello! You've reached Page3!</p>";
+    $("#content").append(html);
+});
